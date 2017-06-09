30c0f9173d04e510e6885187427d829244cdab8f
Muhammad Ridwan Firdaus <va.rhenka@gmail.com>
2017-06-09 21:41:02 +0700

    Add files via upload

---
+++ src/js/myscript.js
@@ -0,0 +1,12 @@
+
+  // Initialize collapse button
+  $ (document).ready(function(){
+      $(".button-collapse").sideNav();
+      $('.materialboxed').materialbox();
+      $('select').material_select();
+       $('.slider').slider();
+});
+        
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+        
